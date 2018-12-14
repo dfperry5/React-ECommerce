518f8e20140eeeed3cb3d402f2265018eeffa147
Dylan Perry <DylanPerry5@gmail.com>
2018-12-13 23:06:33 -0500

    Chapters 1-3 of ECommerce React/Redux Tutorial

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import './main.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension'
+import thunk from 'redux-thunk';
+import {browserHistory, Router, Route} from 'react-router';
+import {syncHistoryWithStore} from 'react-router-redux';
+import {Provider} from 'react-redux';
+
+import Layout from 'containers/layout';
+import Phones from 'containers/phones';
+
+
+// Can import like this b/c we use it in node_modules folder in src.
+import reducers from 'reducers';
+
+// Want to pass root reducer inside here
+const store = createStore(reducers, composeWithDevTools(
+    applyMiddleware(thunk)
+    ));
+
+const history = syncHistoryWithStore(browserHistory, store);
+
+// Parameter one routing
+// Parameter 2 where we inject React
+ReactDOM.render(
+    <Provider store={store}>
+        <Router history={history}>
+            <Route component={Layout}>
+                <Route path ="/" component={Phones} />
+            </Route>
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+)
